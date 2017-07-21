7c18c95817ba11012d0ae566a9d827d851edad6f
dan <daniel.picton@gmail.com>
2017-07-21 06:05:37 +0100

    Added Sieve of Eratosthenes
    First attempt; output as vector of maps.

--- src/algo_fun/core.clj
+++ src/algo_fun/core.clj
@@ -1,5 +1,23 @@
 (ns algo-fun.core
+  (:require [clojure.repl :refer [doc source]])
   (:gen-class))
+
+(defn eratosthenes
+  [n]
+  "Returns map of all primes to n, along with sieved non-primes."
+  (loop [nums     (range 2 (inc n))
+         primes   []
+         curr-fac 2]
+    (let [sieve    #(= (mod % curr-fac) 0)
+          caught   (filter sieve nums)
+          dropped  (remove sieve nums)]
+      (if (empty? nums)
+        primes
+        (recur dropped
+               (into primes
+                     (map #(hash-map :num % :fac curr-fac))
+                     caught)
+               (first nums))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
